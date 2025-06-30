1f577083a0426186d789e75a2295f69994fdda64
kauan <172656707+Kauan-git-profile@users.noreply.github.com>
2025-06-30 19:19:27 -0300

    Update multithreading.c
    Correção de bugs com a apresentação das métricas

--- multithreading.c
+++ multithreading.c
@@ -12,6 +12,7 @@
 #define NUM_ASSENTOS (FILAS * COLUNAS)
 
 int assentos_ocupados = 0;
+int n_registros = 0; // Contador de registros
 sem_t semid;
 
 // Funções
@@ -27,10 +28,13 @@ pthread_t threads[NUM_THREADS];
 
 // Utilizado para registrar assentos no arquivo
 void registrar_ocupante_assento(int fila, int colunas, pthread_t tid){
+
     FILE *f = fopen("ocupantes.txt", "a");
     if (f == NULL) return;
     fprintf(f, "Ocupante do assento [%d][%d]: Thread %lu\n", fila, colunas, tid);
     fclose(f);
+
+    n_registros++;
 }
 
 // Inicializa todos os assentos
@@ -117,19 +121,14 @@ void* alocar_lugar_paralelamente_sync(void* arg){
 
 // Verifica a integridade e escreve métricas
 void checarMetricas(char assentos[FILAS][COLUNAS], const char* metodo, double tempo){
-    int ocupados = 0;
-    for (int i = 0; i < FILAS; i++)
-        for (int j = 0; j < COLUNAS; j++)
-            if (assentos[i][j] == 'O')
-                ocupados++;
 
     double throughput = NUM_ASSENTOS / tempo;
 
     printf("\nMétricas - %s:\n", metodo);
     printf("Tempo de execução: %.6f segundos\n", tempo);
     printf("Vazão (assentos/s): %.2f\n", throughput);
-    printf("Assentos ocupados: %d\n", ocupados);
-    if (ocupados != NUM_ASSENTOS)
+    printf("Assentos ocupados: %d\n", assentos_ocupados);
+    if (n_registros != NUM_ASSENTOS)
         printf("Quantidade incorreta de assentos alocados\n");
     else
         printf("Integridade verificada com sucesso\n");
